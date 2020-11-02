a29739fdb197d4ac78a8ccf974865be626463c48
Phiremouse <Byford.Boyd@gmail.com>
2020-11-01 20:23:16 -0600

    Inital Upload

---
+++ Setting.py
@@ -0,0 +1,10 @@
+class Settings:
+    """ A class to store all settiong for Alien Invastion."""
+    def __init__(self):
+        """initalize the game's setting."""
+        #Screen settings
+        self.screen_width = 1200
+        self.scree_height = 800
+        self.bg_color = (230, 230, 230)
+
+
